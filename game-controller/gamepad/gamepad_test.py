c21bb3eeba420f56fdca42d73e69a14d18fb46c2
timsinaga86 <51060435+timsinaga86@users.noreply.github.com>
2022-12-02 15:33:14 -0600

    Successfully tested vgamepad library

---
+++ game-controller/gamepad/gamepad_test.py
@@ -0,0 +1,63 @@
+#game pad state is initialized as a "report"
+#to press buttons, you modify the report and then call update()
+
+#process is:
+    #button press
+    #update
+    #sleep
+
+    #button release
+    #update
+    #sleep
+
+#one way of doing this could be just to read the state of every button on the microbit constantly
+#and then just pass everything doing in the report
+    
+
+import vgamepad as vg
+import time
+
+gamepad = vg.VX360Gamepad()
+
+while True:
+
+    
+
+# press a button to wake the device up
+    gamepad.press_button(button=vg.XUSB_BUTTON.XUSB_GAMEPAD_A)
+    gamepad.update()
+    time.sleep(0.5)
+    gamepad.release_button(button=vg.XUSB_BUTTON.XUSB_GAMEPAD_A)
+    gamepad.update()
+    time.sleep(0.5)
+
+# press buttons and things
+    gamepad.press_button(button=vg.XUSB_BUTTON.XUSB_GAMEPAD_A)
+    gamepad.press_button(button=vg.XUSB_BUTTON.XUSB_GAMEPAD_LEFT_SHOULDER)
+    gamepad.press_button(button=vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_DOWN)
+    gamepad.press_button(button=vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_LEFT)
+    gamepad.left_trigger_float(value_float=0.5)
+    gamepad.right_trigger_float(value_float=0.5)
+    gamepad.left_joystick_float(x_value_float=0.0, y_value_float=0.2)
+    gamepad.right_joystick_float(x_value_float=-1.0, y_value_float=1.0)
+
+    gamepad.update()
+
+    time.sleep(1.0)
+
+# release buttons and things
+    gamepad.release_button(button=vg.XUSB_BUTTON.XUSB_GAMEPAD_A)
+    gamepad.release_button(button=vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_LEFT)
+    gamepad.right_trigger_float(value_float=0.0)
+    gamepad.right_joystick_float(x_value_float=0.0, y_value_float=0.0)
+
+    gamepad.update()
+
+    time.sleep(1.0)
+
+# reset gamepad to default state
+    gamepad.reset()
+
+    gamepad.update()
+
+    time.sleep(1.0)
